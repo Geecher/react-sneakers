5a6c59b9e9438a7337cd6288ee2537be6536ba74
Geecher <nikita.chugunov96@mail.ru>
2023-01-14 22:42:43 +0400

    add element for sidebar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,45 @@ function App() {
         <div className="overlay">
             <div className="drawer">
                 <h2 className="mb-30">Корзина</h2>
+
+                <div className="items">
+                    <div className="cartItem mb-20">
+                        <div style={{ backgroundImage: 'url(/img/sneakers/001.jpg)' }} className="cartItemImg"></div>
+                        <div className="mr-20">
+                            <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                            <b>12 999 руб.</b>
+                        </div>
+                        <button className="removeBtn">
+                            <img width={11} height={11} src="/img/btn-remove.svg" alt="Plus"/>
+                        </button>
+                    </div>
+                    <div className="cartItem mb-20">
+                        <div style={{ backgroundImage: 'url(/img/sneakers/001.jpg)' }} className="cartItemImg"></div>
+                        <div className="mr-20">
+                            <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                            <b>12 999 руб.</b>
+                        </div>
+                        <button className="removeBtn">
+                            <img width={11} height={11} src="/img/btn-remove.svg" alt="Plus"/>
+                        </button>
+                    </div>
+                </div>
+
+                <div className="cartTotalBlock">
+                    <ul>
+                        <li>
+                            <span>Итого:</span>
+                            <div></div>
+                            <b>21 498 руб.</b>
+                        </li>
+                        <li>
+                            <span>Налог 5%: </span>
+                            <div></div>
+                            <b>1074 руб.</b>
+                        </li>
+                    </ul>
+                    <button>Оформить заказ</button>
+                </div>
             </div>
         </div>
 
